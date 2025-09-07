de02c3e8d4da4144192e7160161931b811fa30f4
Lokesh Bichubar <lokeshsaini339766@gmail.com>
2025-09-07 15:16:05 +0530

    Add files via upload

---
+++ Project 6 sept.py
@@ -0,0 +1,13 @@
+X = str(input("Name: "))
+Y = int(input("Roll no.: "))
+
+# Marks in subjects
+English = int(input("English: "))
+Hindi = int(input("Hindi: "))
+Mathematics = int(input("Mathematics: "))
+Science = int(input("Science: "))
+Physical = int(input("Physical: "))
+
+total = English + Hindi + Mathematics + Science + Physical
+
+print("Total Marks:", total, "out of 500")
